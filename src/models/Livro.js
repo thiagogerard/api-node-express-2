cfeedb21e0f900a2715d3db2eeb910aaad7693db
Thiago Gerard <114416962+thiagogerard@users.noreply.github.com>
2025-10-29 12:27:22 -0700

    feat: refactor model imports and enhance livro schema validation

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -2,24 +2,36 @@ import mongoose from "mongoose";
 
 const livroSchema = new mongoose.Schema(
   {
-    id: {type: String},
+    id: { type: String },
     titulo: {
-      type: String, 
+      type: String,
       required: [true, "O titulo do livro é obrigatório"]
     },
     autor: {
-      type: mongoose.Schema.Types.ObjectId, 
-      ref: 'autores', 
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'autores',
       required: [true, "O(A) autor(a) do livro é obrigatório"]
     },
     editora: {
-      type: String, 
-      required: [true, "A editora do livro é obrigatório"]
+      type: String,
+      required: [true, "A editora do livro é obrigatório"],
+      enum: {
+        values: ["Casa do codigo", "Alura"],
+        message: "A editora {VALUE} nao e um valor permitido"
+      }
     },
-    numeroPaginas: {type: Number}
+    numeroPaginas: {
+      type: Number,
+      validate: {
+          validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        },
+        message: "o numero de paginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"
+      }
+    }
   }
 );
 
-const livros= mongoose.model('livros', livroSchema);
+const livros = mongoose.model('livros', livroSchema);
 
 export default livros;
